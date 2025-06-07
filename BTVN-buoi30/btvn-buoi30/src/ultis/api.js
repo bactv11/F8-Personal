f31ef31b16b3ccea23ae7619c0c1757f3f83f163
bactv <bactvph26425@fpt.edu.vn>
2025-06-07 20:02:03 +0700

    BTVN -buoi30

---
+++ BTVN-buoi30/btvn-buoi30/src/ultis/api.js
@@ -0,0 +1,78 @@
+const BaseUrl = "https://api-todolist-multiuser.onrender.com/Bac/todos"
+// get
+const methodGet = async (endPoint) => {
+    try{
+        const response = await fetch(`${BaseUrl}/${endPoint}`)
+        return await response.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+//post
+const methodPost = async (endpoint,body) => {
+    try{
+        const response = await fetch(`${BaseUrl}/${endpoint}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            })
+        return await response.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+//update all
+const methodPut = async (endpoint,body) => {
+    try {
+        const response = await fetch(`${BaseUrl}/${endpoint}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            })
+        return await response.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+// patch
+const methodPatch = async (endpoint,body) => {
+    try{
+        const response = await fetch(`${BaseUrl}/${endpoint}`,
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            })
+        return await response.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+//delete
+const methodDelete = async (endpoint,body) => {
+    try {
+        const response = await fetch(`${BaseUrl}/${endpoint}`,
+            {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+        return await response.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+
+//export
+export {
+    methodGet,methodPost,methodPut,methodPatch,methodDelete
+}
