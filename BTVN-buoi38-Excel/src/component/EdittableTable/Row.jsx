e396393b531934c4867c8d077b15131e0af1dc39
bactv <bactvph26425@fpt.edu.vn>
2025-07-03 01:07:53 +0700

    BTVN-buoi39-Excel

--- BTVN-buoi38-Excel/src/component/EdittableTable/Row.jsx
+++ BTVN-buoi38-Excel/src/component/EdittableTable/Row.jsx
@@ -2,17 +2,25 @@ import {useContext} from "react";
 import {TableContext} from "./const.js";
 import Cell from "./Cell.jsx";
 
-export  default function ({row}) {
+export default function ({row, rowIndex}) {
 
     const injector = useContext(TableContext)
 
     const {columns} = injector
 
-    return(
+    return (
         <tr>
             {
-                columns.map(column => {
-                    return <Cell key={column.name} row={row} column={column}/>
+                columns.map((column, index) => {
+                    return (
+                        <Cell
+                            rowIndex={rowIndex}
+                            columnIndex={index}
+                            key={column.name}
+                            row={row}
+                            column={column}
+                        />
+                    )
                 })
             }
         </tr>
--- BTVN-buoi38-Excel/src/component/EdittableTable/index.jsx
+++ BTVN-buoi38-Excel/src/component/EdittableTable/index.jsx
@@ -1,44 +1,57 @@
 import './style.sass'
-import Row from "./Row.jsx";
-import { TableContext } from "./const.js";
+import {TableContext} from "./const.js";
 import {useState} from "react";
+import Row from "./Row.jsx";
+import CellSelection from "./CellSelection.jsx";
+import CellInput from "./CellInput.jsx";
 
 const defaultCursors = {
-    rowIndex:0,
-    columnIndex:0,
+    rowIndex: 0,
+    columnIndex: 0,
     top: 0,
     left: 0,
+    width: 0,
+    height: 0,
+    isEditing: false
 }
-
-export default function({columns,rows}) {
-    const [cursor,setCursor] = useState({...defaultCursors})
-
-    console.log(cursor)
+export default function ({columns, rows}) {
+    const [cursor, setCursor] = useState({...defaultCursors})
 
     const providers = {
-        columns,rows,cursor,setCursor
-    };
-    return(
+        columns, rows, cursor, setCursor
+    }
+
+    const onKeyDown = () => {
+        console.log('onKeyDown')
+    }
+
+    return (
         <TableContext value={providers}>
-            <table className={'editable-table'}>
-                <thead>
-                <tr>
+            <div style={{position: 'relative'}}>
+                <table className={'editable-table'} onKeyDown={onKeyDown}>
+                    <thead>
+                    <tr>
+                        {
+                            columns.map(c => {
+                                return <th key={c.name}>{c.name}</th>
+                            })
+                        }
+                    </tr>
+
+                    </thead>
+                    <tbody>
                     {
-                        columns.map(column => {
-                            return <th key={column.name}>{column.name}</th>
+                        rows.map((row, index) => {
+                            return (
+                                <Row key={row.product} row={row} rowIndex={index}/>
+                            )
                         })
                     }
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    rows.map(row => {
-                        return <Row key={row.product} row={row}/>
-                    })
-                }
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+                <CellInput/>
+                <CellSelection/>
+            </div>
         </TableContext>
-
     )
 }
